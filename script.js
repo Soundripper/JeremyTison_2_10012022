55f7e51a259845fb5b29214226e5184ffd668a28
Soundripper <jerrsoundripper@gmail.com>
2022-02-08 15:17:52 +0100

    Après session P2_2

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-document.addEventListener('click', function () {
-
-	// Bail if it's not a .nav-link
-	if (!event.target.classList.contains('nav-link')) return;
-
-	// Add the active class
-	event.target.classList.add('active');
-
-	// Get all nav links
-	var links = document.querySelectorAll('.nav-link');
-
-	// Loop through each link
-	for (var i = 0; i < links.length; i++) {
-
-		// If the link is the one clicked, skip it
-		if (links[i] === event.target) continue;
-
-		// Remove the .active class
-		links[i].classList.remove('active');
-
+function toggleActive() {
+	if (document.querySelector('a.active') !== null) {
+	  document.querySelector('a.active').classList.remove('active');
 	}
-
-}, false);
+	event.target.className = "active";
+  }
